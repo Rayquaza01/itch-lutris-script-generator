1aabb8fca1a64e84147361d5c6dfd97694c89205
Joe Jarvis <joe.jarvis109@gmail.com>
2023-05-23 18:44:49 -0400

    Get endpoint from metadata

--- itch-lutris.py
+++ itch-lutris.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
 import requests
 import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument("url", help="The URL of the game from itch.io")
+ap.add_argument("--api-key", help="The API key for your account")
 args = ap.parse_args()
 
-itchpage = requests.get(args.url).content
+itchpage = requests.get(args.url + "/data.json").json()
+print(itchpage["id"])
 
-parser = BeautifulSoup(itchpage, "html.parser")
-id = parser.select_one('meta[name="itch:path"]').get("content").split("/")[1]
-print(id)
+# TODO
+# https://github.com/lutris/lutris/blob/27905d75b99bcb6cdaeef0d9a6dd5c388ed1a73e/lutris/services/itchio.py#L309
