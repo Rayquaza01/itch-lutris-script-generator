5b8ced53081ba9d16a25499161da9f176ea02957
Joe Jarvis <joe.jarvis109@gmail.com>
2023-05-23 18:52:24 -0400

    Pull title and id from metadata

--- itch-lutris.py
+++ itch-lutris.py
@@ -7,7 +7,12 @@ ap.add_argument("--api-key", help="The API key for your account")
 args = ap.parse_args()
 
 itchpage = requests.get(args.url + "/data.json").json()
-print(itchpage["id"])
+
+id = str(itchpage["id"])
+name = itchpage["title"]
+print(id + " " + name)
+
+uploads = requests.get("https://itch.io/api/1/" + args.api_key + "/game/" + id + "/uploads").json()
 
 # TODO
 # https://github.com/lutris/lutris/blob/27905d75b99bcb6cdaeef0d9a6dd5c388ed1a73e/lutris/services/itchio.py#L309
