c99178b76f7a0669662d887b186a74f7a3acf4ef
Joe Jarvis <joe.jarvis109@gmail.com>
2023-05-23 18:57:39 -0400

    Get install candidates

--- itch-lutris.py
+++ itch-lutris.py
@@ -14,5 +14,16 @@ print(id + " " + name)
 
 uploads = requests.get("https://itch.io/api/1/" + args.api_key + "/game/" + id + "/uploads").json()
 
+linux = [i for i in uploads["uploads"] if i["p_linux"]]
+if len(linux) > 0:
+    print(linux[0])
+else:
+    windows = [i for i in uploads["uploads"] if i["p_windows"]]
+    if (len(windows) > 0):
+        print(windows[0])
+    else:
+        # no versions available
+        exit()
+
 # TODO
 # https://github.com/lutris/lutris/blob/27905d75b99bcb6cdaeef0d9a6dd5c388ed1a73e/lutris/services/itchio.py#L309
